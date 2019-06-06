ccd709460ac111ebf1611187928d19dfd1a746b2
Danielle Mizrachi <dmiz@brandeis.edu>
2019-06-06 11:21:54 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,6 +1,6 @@
 class CashRegister
   #creating a blueprint for cash resgister - each will be an object with its own properties 
-  
+  #What do we need to do first? 
   read_accessor 
   
   def initialize
