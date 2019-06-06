b9d3eaaa054e9e9d3f349b357b75b112deb41bb4
Danielle Mizrachi <dmiz@brandeis.edu>
2019-06-06 11:22:19 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,10 +1,7 @@
 class CashRegister
-  #creating a blueprint for cash resgister - each will be an object with its own properties 
-  #What do we need to do first? 
-  read_accessor 
-  
-  def initialize
-  
+
+  def initialize(total)
+    @total=total 
   end 
   
 end 
